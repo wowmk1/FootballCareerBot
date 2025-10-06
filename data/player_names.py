91184d9e49f3cad20521eaf84cd6b96704d8923a
wowmk1 <91302977+wowmk1@users.noreply.github.com>
2025-10-06 19:32:28 -0400

    Create player_names.py

---
+++ data/player_names.py
@@ -0,0 +1,111 @@
+# Expanded Player Name Database for Regens
+
+FIRST_NAMES = [
+    # English/British Names
+    "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph",
+    "Thomas", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven",
+    "Paul", "Andrew", "Joshua", "Kenneth", "George", "Kevin", "Brian", "Edward",
+    "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephen", "Larry",
+    "Jack", "Harry", "Charlie", "Oliver", "Oscar", "Alfie", "Noah", "Connor",
+    "Lewis", "Jake", "Callum", "Ben", "Luke", "Alex", "Max", "Sam", "Tom", "Joe",
+    "Adam", "Aaron", "Adrian", "Alan", "Albert", "Alexander", "Andre", "Angel",
+    "Antonio", "Arthur", "Austin", "Benjamin", "Blake", "Brandon", "Brett", "Bruce",
+    "Carlos", "Carl", "Chad", "Charles", "Christian", "Cody", "Cole", "Colin",
+    
+    # Spanish/Portuguese Names
+    "Diego", "Carlos", "Jose", "Juan", "Luis", "Miguel", "Pedro", "Fernando",
+    "Rafael", "Jorge", "Manuel", "Antonio", "Francisco", "Javier", "Sergio", "Pablo",
+    "Alejandro", "Roberto", "Eduardo", "Gabriel", "Marcos", "Daniel", "Hector",
+    "Raul", "Andres", "Felipe", "Cristiano", "Bruno", "Bernardo", "Joao", "Goncalo",
+    "Diogo", "Ricardo", "Andre", "Ruben", "Tiago", "Nuno", "Vitor", "Mateo",
+    
+    # French Names
+    "Antoine", "Alexandre", "Pierre", "Jean", "Luc", "Marc", "Philippe", "Laurent",
+    "Olivier", "Nicolas", "Julien", "Sebastien", "Maxime", "Hugo", "Louis", "Theo",
+    "Lucas", "Nathan", "Leo", "Mathis", "Enzo", "Nolan", "Raphael", "Clement",
+    
+    # German/Dutch Names
+    "Klaus", "Hans", "Franz", "Werner", "Stefan", "Andreas", "Jurgen", "Matthias",
+    "Lars", "Sven", "Erik", "Felix", "Leon", "Lukas", "Jonas", "Florian", "Tobias",
+    "Marco", "Robin", "Dominik", "Arjen", "Daley", "Wesley", "Vincent", "Ruud",
+    
+    # Italian Names
+    "Marco", "Luca", "Alessandro", "Andrea", "Matteo", "Lorenzo", "Simone", "Francesco",
+    "Davide", "Stefano", "Federico", "Nicola", "Tommaso", "Filippo", "Giovanni",
+    
+    # Scandinavian Names
+    "Magnus", "Erik", "Anders", "Lars", "Bjorn", "Henrik", "Oscar", "Mikael",
+    "Niklas", "Emil", "Viktor", "Soren", "Rasmus", "Christian", "Johan",
+    
+    # Eastern European Names
+    "Aleksandr", "Dmitri", "Sergei", "Ivan", "Viktor", "Andrei", "Nikolai", "Pavel",
+    "Artem", "Roman", "Mikhail", "Oleg", "Alexei", "Stanislav", "Yuri",
+    "Luka", "Matej", "Marko", "Jan", "Petr", "Jakub", "Tomas", "Milan",
+    
+    # African Names
+    "Kwame", "Kofi", "Yaw", "Ade", "Chidi", "Kojo", "Sekou", "Amadou",
+    "Ousmane", "Mamadou", "Ibrahima", "Moussa", "Bakary", "Youssef",
+    
+    # Additional Modern Names
+    "Kai", "Zain", "Nico", "Leo", "Finn", "Mason", "Tyler", "Dylan",
+    "Ethan", "Logan", "Jackson", "Aiden", "Jayden", "Liam", "Mason", "Noah"
+]
+
+LAST_NAMES = [
+    # English/British Surnames
+    "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis",
+    "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson",
+    "Thomas", "Taylor", "Moore", "Jackson", "Martin", "Lee", "Thompson", "White",
+    "Harris", "Clark", "Lewis", "Robinson", "Walker", "Young", "Allen", "King",
+    "Wright", "Scott", "Green", "Baker", "Adams", "Nelson", "Carter", "Mitchell",
+    "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins",
+    "Stewart", "Morris", "Murphy", "Cook", "Rogers", "Morgan", "Peterson", "Cooper",
+    "Reed", "Bailey", "Bell", "Gomez", "Kelly", "Howard", "Ward", "Cox", "Diaz",
+    "Richardson", "Wood", "Watson", "Brooks", "Bennett", "Gray", "James", "Reyes",
+    "Hughes", "Price", "Myers", "Long", "Foster", "Sanders", "Ross", "Henderson",
+    "Coleman", "Jenkins", "Perry", "Powell", "Russell", "Sullivan", "Bell", "Barnes",
+    
+    # Spanish/Portuguese Surnames
+    "Silva", "Santos", "Oliveira", "Costa", "Pereira", "Ferreira", "Rodrigues", "Alves",
+    "Fernandez", "Sanchez", "Ramirez", "Torres", "Flores", "Rivera", "Morales",
+    "Jimenez", "Ruiz", "Cruz", "Ortiz", "Gomez", "Diaz", "Castillo", "Vargas",
+    "Mendoza", "Guerrero", "Aguilar", "Medina", "Rojas", "Vega", "Alonso",
+    
+    # French Surnames
+    "Dubois", "Durand", "Leroy", "Moreau", "Simon", "Laurent", "Lefebvre", "Michel",
+    "Garcia", "David", "Bertrand", "Roux", "Vincent", "Fournier", "Girard", "Bonnet",
+    "Dupont", "Lambert", "Fontaine", "Rousseau", "Blanc", "Garnier", "Faure", "Mercier",
+    
+    # German/Dutch Surnames
+    "Müller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Wagner", "Becker",
+    "Schulz", "Hoffmann", "Koch", "Bauer", "Richter", "Klein", "Wolf", "Schröder",
+    "Van Dijk", "De Jong", "Bakker", "Jansen", "Visser", "Smit", "De Vries", "Hoek",
+    
+    # Italian Surnames
+    "Rossi", "Russo", "Ferrari", "Esposito", "Bianchi", "Romano", "Colombo", "Ricci",
+    "Marino", "Greco", "Bruno", "Gallo", "Conti", "De Luca", "Mancini", "Costa",
+    
+    # Scandinavian Surnames
+    "Andersson", "Johansson", "Eriksson", "Larsson", "Olsson", "Persson", "Nilsson",
+    "Gustafsson", "Pettersson", "Jonsson", "Karlsson", "Lindberg", "Hansen", "Nielsen",
+    
+    # Eastern European Surnames
+    "Novak", "Kovac", "Horvat", "Petrovic", "Jovanovic", "Popov", "Ivanov", "Petrov",
+    "Sokolov", "Lebedev", "Kuznetsov", "Vasiliev", "Pavlov", "Smirnov", "Volkov",
+    
+    # African Surnames
+    "Mensah", "Osei", "Boateng", "Asamoah", "Appiah", "Owusu", "Addo", "Asante",
+    "Diallo", "Toure", "Traore", "Keita", "Camara", "Kone", "Coulibaly", "Diarra",
+    
+    # Additional Common Surnames
+    "Kane", "Cole", "Owen", "Stone", "Palmer", "Fletcher", "Rhodes", "Cross",
+    "Gibson", "Hunt", "Webb", "Chapman", "Boyd", "Freeman", "Wells", "Tucker",
+    "Harvey", "Armstrong", "Ferguson", "Graham", "Shaw", "Payne", "Riley"
+]
+
+def get_random_player_name():
+    """Generate a random player name from the expanded database"""
+    import random
+    first = random.choice(FIRST_NAMES)
+    last = random.choice(LAST_NAMES)
+    return f"{first} {last}"
