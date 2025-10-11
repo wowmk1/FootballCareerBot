48f5a1d5fbed6af5b2ad699f68fb483afed9c7db
wowmk1 <91302977+wowmk1@users.noreply.github.com>
2025-10-11 10:34:42 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Activate virtual environment if you want (optional)
-# source venv/bin/activate
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run your bot
-python bot.py
