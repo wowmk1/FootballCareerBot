d44f108c24f42833ed0b73e76a94789adbb70573
wowmk1 <91302977+wowmk1@users.noreply.github.com>
2025-10-11 10:23:47 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Activate virtual environment if you want (optional)
+# source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run your bot
+python bot.py
